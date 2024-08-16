0fff11a271615ad741bb9fbc6f9e22fa42f2848b
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-08-16 12:20:44 -0500

    Setup dev environment

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  pluginJs.configs.recommended,
+  { languageOptions: { globals: globals.browser } },
+  {
+    files: ["babel.config.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
+  {
+    files: ["*.test.js"],
+    env: {
+      jest: true,
+    },
+  },
+];
---
+++ index.test.js
@@ -0,0 +1,5 @@
+import hello from "./index.js";
+
+test("Jest works", () => {
+  expect(hello()).toBe("hello");
+});
