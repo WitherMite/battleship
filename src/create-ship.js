1e995e6f1767df92457de4031f9c7bc24cc366bb
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-08-17 14:09:30 -0500

    Create specific ship types

--- src/create-ship.js
+++ src/create-ship.js
@@ -1,12 +1,21 @@
-export default function createShip(length) {
+export default function createShip(type) {
+  const shipTypes = {
+    carrier: { name: "carrier", length: 5 },
+    battleship: { name: "battleship", length: 4 },
+    destroyer: { name: "destroyer", length: 3 },
+    submarine: { name: "submarine", length: 3 },
+    patrol: { name: "patrol", length: 2 },
+  };
+
   return {
-    length,
     hits: 0,
     isSunk() {
       return this.hits >= this.length;
     },
     hit() {
       this.hits++;
+      return true;
     },
+    ...shipTypes[type],
   };
 }
--- src/create-ship.test.js
+++ src/create-ship.test.js
@@ -1,28 +1,45 @@
 import createShip from "./create-ship.js";
 
 describe("Create valid ship objects", () => {
-  const ship = createShip(4);
-  test("Has correct length", () => {
-    expect(ship.length).toBe(4);
-  });
+  const ship = createShip("battleship");
   test("Has correct initial hits", () => {
     expect(ship.hits).toBe(0);
   });
   test("Starts not sunk", () => {
     expect(ship.isSunk()).toBe(false);
   });
+});
 
-  const ship2 = createShip(2);
-  const ship3 = createShip(5);
-  test("Length is customizable", () => {
-    expect(ship2.length).toBe(2);
-    expect(ship3.length).toBe(5);
+describe("Creates different types of boats", () => {
+  test.each([
+    ["carrier", 5],
+    ["battleship", 4],
+    ["destroyer", 3],
+    ["submarine", 3],
+    ["patrol", 2],
+  ])("Creates %s", (name, len) => {
+    const ship = createShip(name);
+    expect(ship.length).toBe(len);
+    expect(ship.name).toBe(name);
   });
 });
 
-describe("Ships can be sunk", () => {
-  const patrol = createShip(2);
-  const carrier = createShip(5);
+describe("Ships can be hit", () => {
+  const ship = createShip("battleship");
+  const isHit = ship.hit();
+
+  test("Ship tracks hits", () => {
+    ship.hit();
+    expect(ship.hits).toBe(2);
+  });
+  test("Returns true when hit", () => {
+    expect(isHit).toBe(true);
+  });
+});
+
+describe("Ships are sunk when hits equal length", () => {
+  const patrol = createShip("patrol");
+  const carrier = createShip("carrier");
   test("Sink patrol", () => {
     patrol.hit();
     patrol.hit();
