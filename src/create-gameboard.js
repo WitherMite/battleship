fcb961f057f2453166f269fa0e2826e4740dec09
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-08-18 20:23:23 -0500

    Place ships on gameboard

--- src/create-gameboard.js
+++ src/create-gameboard.js
@@ -1,9 +1,36 @@
+import createShip from "./create-ship.js";
+
 export default function createGameboard() {
   const gameboard = Array.from({ length: 10 }, () =>
     Array.from({ length: 10 }, () => ({ ship: null, isHit: false }))
   );
 
+  function placeShip(coordinates, direction, shipType) {
+    const ship = createShip(shipType);
+    const [x, y] = [...coordinates];
+    const isYAxis = direction === "up" || direction === "down";
+
+    // board is a 2d array so x axis is a bit more complicated to place on as you have to get the same index out of multiple arrays
+    if (isYAxis) {
+      const shipTiles = gameboard[x].slice(...sliceShipSize(y));
+      shipTiles.forEach((tile) => (tile.ship = ship));
+    } else {
+      const shipTiles = gameboard
+        .slice(...sliceShipSize(x))
+        .map((row) => row[y]);
+      shipTiles.forEach((tile) => (tile.ship = ship));
+    }
+
+    function sliceShipSize(coord) {
+      const isPositiveFacing = direction === "up" || direction === "right";
+      return isPositiveFacing
+        ? [coord, coord + ship.length + 1]
+        : [coord - ship.length, coord + 1];
+    }
+  }
+
   return {
+    placeShip,
     getState() {
       const boardDeepCopy = JSON.parse(JSON.stringify(gameboard));
       return boardDeepCopy;
--- src/create-gameboard.test.js
+++ src/create-gameboard.test.js
@@ -18,3 +18,33 @@ describe("Create valid gameboard objects", () => {
     expect(hasValidTiles).toBe(true);
   });
 });
+
+describe("Ships can be placed on gameboards", () => {
+  let board;
+  beforeEach(() => (board = createGameboard()));
+
+  test.each([
+    ["up", 3, 0],
+    ["down", 4, 4],
+  ])("Ship pointed %s", (direction, x, y) => {
+    board.placeShip([x, y], direction, "battleship");
+    const boardState = board.getState();
+    const shipTiles = boardState[x].slice(0, y + 1);
+
+    expect(shipTiles.every((tile) => tile.ship.name === "battleship")).toBe(
+      true
+    );
+  });
+  test.each([
+    ["left", 4, 4],
+    ["right", 0, 3],
+  ])("Ship pointed %s", (direction, x, y) => {
+    board.placeShip([x, y], direction, "battleship");
+    const boardState = board.getState();
+    const shipTiles = boardState.slice(0, x + 1).map((row) => row[y]);
+
+    expect(shipTiles.every((tile) => tile.ship.name === "battleship")).toBe(
+      true
+    );
+  });
+});
